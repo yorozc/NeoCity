72c942fd11fadc6e38a738ede5edd31484bf0b72
yahir <you@example>
2025-03-30 14:05:39 -0700

    fixed the doors automatically opening when website is opened

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const leftDoor = document.querySelector(".left-door");
 const rightDoor = document.querySelector(".right-door");
 const botLeft = document.querySelector(".bot-left");
 const botRight = document.querySelector(".bot-right");
+let isOpen = false;
+
 gate.addEventListener("click", ()=>{
     console.log("Clicked");
     leftDoor.style.animationName = "openLeft";
     rightDoor.style.animationName = "openRight";
     botLeft.style.animationName = "openLeft";
     botRight.style.animationName = "openRight";
+    isOpen = true
+
 });
